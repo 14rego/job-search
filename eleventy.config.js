217e2ef70ad44424b62780c4f089b210a91dc137
Regan Bourland <1775321+14rego@users.noreply.github.com>
2025-12-24 17:52:04 -0600

    need to deploy to create PDF

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,37 +3,37 @@ import moment from "moment";
 moment.locale("en");
 
 export default function (eleventyConfig) {  
-  eleventyConfig.setInputDirectory("src");
-  //eleventyConfig.setIncludesDirectory("_includes"); // default
-  //eleventyConfig.setDataDirectory("_data"); // default
-  eleventyConfig.setLayoutsDirectory("_layouts");
-  eleventyConfig.setOutputDirectory("docs");
+    eleventyConfig.setInputDirectory("src");
+    //eleventyConfig.setIncludesDirectory("_includes"); // default
+    //eleventyConfig.setDataDirectory("_data"); // default
+    eleventyConfig.setLayoutsDirectory("_layouts");
+    eleventyConfig.setOutputDirectory("docs");
 
-  eleventyConfig.addPassthroughCopy("src/assets/style");
-  eleventyConfig.addPassthroughCopy("src/assets/image");
+    eleventyConfig.addPassthroughCopy("src/assets/style");
+    eleventyConfig.addPassthroughCopy("src/assets/image");
 
-  eleventyConfig.addFilter("yyyy", (d) => {
-    return moment(d).format('YYYY');
-  });
-  eleventyConfig.addFilter("mmyyyy", (d) => {
-    return moment(d).format('MM/YYYY');
-  });
-  eleventyConfig.addFilter("mmmyyyy", (d) => {
-    return moment(d).format('MMM. YYYY');
-  });
-  eleventyConfig.addFilter("azTitle", (arr) => {
-    return arr.sort((a, b) => a.title > b.title ? 1 : -1);
-  });
-  eleventyConfig.addFilter("dateAsc", (arr) => {
-    return arr.sort((a, b) => moment().unix(a.start) > moment(b.start).unix() ? 1 : -1);
-  });
-  eleventyConfig.addFilter("dateDesc", (arr) => {
-    return arr.sort((a, b) => moment().unix(a.start) > moment(b.start).unix() ? -1 : 1);
-  });
-  eleventyConfig.addFilter("preference", (arr) => {
-    return arr.sort((a, b) => a.preference > b.preference ? -1 : 1);
-  });
-  eleventyConfig.addFilter("years", (arr) => {
-    return arr.sort((a, b) => a.preference > b.preference ? -1 : 1);
-  });
+    eleventyConfig.addFilter("yyyy", (d) => {
+        return moment(d).format("YYYY");
+    });
+    eleventyConfig.addFilter("mmyyyy", (d) => {
+        return moment(d).format("MM/YYYY");
+    });
+    eleventyConfig.addFilter("mmmyyyy", (d) => {
+        return moment(d).format("MMM. YYYY");
+    });
+    eleventyConfig.addFilter("azTitle", (arr) => {
+        return arr.sort((a, b) => a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1);
+    });
+    eleventyConfig.addFilter("dateAsc", (arr) => {
+        return arr.sort((a, b) => a.start && b.start && moment(a.start).format("x") > moment(b.start).format("x") ? 1 : -1);
+    });
+    eleventyConfig.addFilter("dateDesc", (arr) => {
+        return arr.sort((a, b) => a.start && b.start && moment(a.start).format("x") > moment(b.start).format("x") ? -1 : 1);
+    });
+    eleventyConfig.addFilter("preference", (arr) => {
+        return arr.sort((a, b) => a.preference > b.preference ? -1 : 1);
+    });
+    eleventyConfig.addFilter("years", (arr) => {
+        return arr.sort((a, b) => a.preference > b.preference ? -1 : 1);
+    });
 };
